3f2190618f795bd2b8b3fff9f964a1045c45e391
Maruf OVi <137372623+iamovi@users.noreply.github.com>
2024-02-05 18:34:18 +0600

    Update script.js

--- editor/script.js
+++ editor/script.js
@@ -1,21 +1,21 @@
-// Include marked.js library for parsing Markdown content
 const markedScript = document.createElement('script');
 markedScript.src = 'https://cdn.jsdelivr.net/npm/marked@2.1.3/marked.min.js';
 document.head.appendChild(markedScript);
 
-
-// Function to toggle preview and handle copy button visibility
+// Function to toggle preview and handle copy and download buttons visibility
 function togglePreview() {
     const previewContainer = document.getElementById('previewContainer');
     const readmeEditor = document.getElementById('readmeEditor');
     const previewButton = document.querySelector('.preBtn');
     const copyButton = document.querySelector('.copyBtn');
+    const downloadButton = document.querySelector('.downloadBtn');
 
     if (previewContainer.style.display === 'none') {
         // Display preview
         previewContainer.style.display = 'block';
         readmeEditor.style.display = 'none';
         copyButton.style.display = 'none'; // Hide the copy button during preview
+        downloadButton.style.display = 'none'; // Hide the download button during preview
 
         // Use marked.js to render Markdown content
         const markedContent = marked(readmeEditor.value);
@@ -36,11 +36,24 @@ function togglePreview() {
         // Hide preview
         previewContainer.style.display = 'none';
         readmeEditor.style.display = 'block';
-        copyButton.style.display = 'block'; // Show the copy button when hiding preview
+        copyButton.style.display = 'inline'; // Show the copy button when hiding preview
+        downloadButton.style.display = 'inline'; // Show the download button when hiding preview
 
         // Change button text back to "Preview It"
         previewButton.innerHTML = 'Preview It';
     }
+}
+
+// Function to download README.md file
+function downloadReadme() {
+    const readmeContent = document.getElementById('readmeEditor').value;
+    const blob = new Blob([readmeContent], { type: 'text/plain' });
+    const a = document.createElement('a');
+    a.href = window.URL.createObjectURL(blob);
+    a.download = 'README.md';
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
 }
 
 // Initialize clipboard.js for copy button
